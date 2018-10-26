38f08757f72b9ace974f67400c2622c4978f19b3
Ricardo Alves <31485246+RicardoMVA@users.noreply.github.com>
2018-10-26 14:39:57 -0300

    made 'print_symbol' function, cleaned redundant code
    'print_symbol' is now an agnostic function, and takes the 'current_symbol' chosen by the 'current_player', as per definition in the 'player_turn' function inside 'tic tac toe.py' file

--- graphics.py
+++ graphics.py
@@ -15,57 +15,27 @@ def board_start():
     return board
 
 
-def circle(str):
-	global line_3, line_5, line_6
-	global board
+def print_symbol(play, symbol):
+	global line_3, line_5, line_6, board
 
-	if str == 'a1':
-		line_3 = line_3[:8] + 'O' + line_3[9:]
-	elif str == 'a2':
-		line_5 = line_5[:8] + 'O' + line_5[9:]
-	elif str == 'a3':
-		line_6 = line_6[:8] + 'O' + line_6[9:]
-	elif str == 'b1':
-		line_3 = line_3[:16] + 'O' + line_3[17:]
-	elif str == 'b2':
-		line_5 = line_5[:16] + 'O' + line_5[17:]
-	elif str == 'b3':
-		line_6 = line_6[:16] + 'O' + line_6[17:]
-	elif str == 'c1':
-		line_3 = line_3[:24] + 'O' + line_3[25:]
-	elif str == 'c2':
-		line_5 = line_5[:24] + 'O' + line_5[25:]
-	elif str == 'c3':
-		line_6 = line_6[:24] + 'O' + line_6[25:]
-
-	board = (line_0 + line_1 + line_2 + line_3
-    	         + line_4 + line_2 + line_5 + line_4
-    	         + line_2 + line_6 + line_2 + line_0)
-	return board
-
-
-def ex(str):
-	global line_3, line_5, line_6
-	global board
-
-	if str == 'a1':
-		line_3 = line_3[:8] + 'X' + line_3[9:]
-	elif str == 'a2':
-		line_5 = line_5[:8] + 'X' + line_5[9:]
-	elif str == 'a3':
-		line_6 = line_6[:8] + 'X' + line_6[9:]
-	elif str == 'b1':
-		line_3 = line_3[:16] + 'X' + line_3[17:]
-	elif str == 'b2':
-		line_5 = line_5[:16] + 'X' + line_5[17:]
-	elif str == 'b3':
-		line_6 = line_6[:16] + 'X' + line_6[17:]
-	elif str == 'c1':
-		line_3 = line_3[:24] + 'X' + line_3[25:]
-	elif str == 'c2':
-		line_5 = line_5[:24] + 'X' + line_5[25:]
-	elif str == 'c3':
-		line_6 = line_6[:24] + 'X' + line_6[25:]
+	if play == 'a1':
+		line_3 = line_3[:8] + symbol + line_3[9:]
+	elif play == 'a2':
+		line_5 = line_5[:8] + symbol + line_5[9:]
+	elif play == 'a3':
+		line_6 = line_6[:8] + symbol + line_6[9:]
+	elif play == 'b1':
+		line_3 = line_3[:16] + symbol + line_3[17:]
+	elif play == 'b2':
+		line_5 = line_5[:16] + symbol + line_5[17:]
+	elif play == 'b3':
+		line_6 = line_6[:16] + symbol + line_6[17:]
+	elif play == 'c1':
+		line_3 = line_3[:24] + symbol + line_3[25:]
+	elif play == 'c2':
+		line_5 = line_5[:24] + symbol + line_5[25:]
+	elif play == 'c3':
+		line_6 = line_6[:24] + symbol + line_6[25:]
 
 	board = (line_0 + line_1 + line_2 + line_3
     	         + line_4 + line_2 + line_5 + line_4
